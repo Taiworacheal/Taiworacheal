44e08af82ed17647867ff246de5d0ff1c74d6cf1
Taiworacheal <bunmyte2020@gmail.com>
2024-08-22 11:50:41 +0100

    Add files via upload

---
+++ Racheal folder/racchy.js
@@ -0,0 +1,28 @@
+console.log (typeof 35);
+console.log (20 + 50);
+console.log (typeof Richeli);
+console.log (typeof "Racchy");
+console.log ("20" + "50");
+console.log ( "Big " + "man");
+console.log ("20" - "50");
+let a=10, b=25;
+console.log (a+b);
+console.log (a-b);
+console.log (a=b);
+console.log (a*b);
+console.log (a/b);
+console.log (b%a);
+console.log (a>b);
+console.log (a<b);
+console.log (a==b);
+console.log (a<=b);
+console.log (a>=b);
+console.log (a++);
+console.log (b--);
+let name = "rachhy";
+const greeting = "hello";
+console.log (greeting  + name)
+
+let userName=String(prompt("Rachy"));
+console.log("Hello" + userName)
+
